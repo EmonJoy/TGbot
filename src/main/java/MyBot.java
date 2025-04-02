d2b0523427cfdd4d1e86d1818858e5fa57a15111
MR. Morningstar <ee8991471@gmail.com>
2025-04-03 01:35:15 +0600

    Initial commit

---
+++ src/main/java/MyBot.java
@@ -0,0 +1,68 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import java.io.IOException;
+
+public class MyBot extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotUsername() {
+        return "coder22bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "6817651266:AAHDzZjfl3JyMhubKWcg-_hmTvqiCPw1JSY";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String text = update.getMessage().getText().trim();
+            long chatId = update.getMessage().getChatId();
+
+            String weatherInfo;
+            try {
+                weatherInfo = WeatherDetails.getWeatherInfo(text);
+            } catch (IOException | InterruptedException e) {
+                weatherInfo = "⚠️ Error";
+            }
+
+            SendMessage message = new SendMessage();
+            message.setChatId(String.valueOf(chatId));
+           // message.setText(weatherInfo);
+            if (text.equalsIgnoreCase("/About")) {
+                message.setText("\uD83C\uDF2A\uFE0F WeatherBot by EmonJoy! \uD83D\uDE80" +
+                        " Type any city, get real-time weather—temp, humidity, " +
+                        "vibes! Stay fresh, stay updated! \uD83D\uDD25☁\uFE0F");
+            } else {
+                message.setText(weatherInfo);
+            }
+
+
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+
+
+
+
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new MyBot());
+            System.out.println("Bot is running...");
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/WeatherDetails.java
@@ -0,0 +1,47 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class WeatherDetails {
+
+    public static String getWeatherInfo(String city) throws IOException, InterruptedException {
+        String apiKey = "d012fe60b0a968b0ee30ac271515f031";
+        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(url))
+                .build();
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        JSONObject json = new JSONObject(response.body());
+
+        if (json.has("main")) {
+            JSONObject main = json.getJSONObject("main");
+            double temp = main.getDouble("temp") - 273.15; // Kelvin to Celsius er formula
+            int humidity = main.getInt("humidity");
+            int pressure = main.getInt("pressure");
+
+            JSONArray weatherArray = json.getJSONArray("weather");
+            JSONObject weather = weatherArray.getJSONObject(0);
+
+            String description = weather.getString("description");
+            String clouds = weather.getString("main");
+            String cityName = json.optString("name", city);
+
+            return "📍 City: " + cityName + "\n" +
+                    "🌡 Temperature: " + String.format("%.2f", temp) + " °C\n" +
+                    "💧 Humidity: " + humidity + "%\n" +
+                    "☁️ Weather: " + clouds + " (" + description + ")\n" +
+                    "⚡ Pressure: " + pressure + " hPa";
+        } else {
+            return "❌ Not found";
+        }
+    }
+}
